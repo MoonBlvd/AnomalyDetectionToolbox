da3d579712d61293cb36525209bf8a23b3b5d477
brianyao <brianyao@umich.edu>
2017-03-06 11:29:17 -0500

    add multi clusters

--- Algorithms/ewIDCAD/ewIDCAD.py
+++ Algorithms/ewIDCAD/ewIDCAD.py
@@ -1,7 +1,7 @@
 import numpy as np
 import csv
-from onlineUpdate import onlineUpdate
-from classifier import classifier
+from onlineUpdate import onlineUpdator
+from classifier import anomalyDetector
 
 def dataReader(file_path):
     # The read-in data should be a N*W matrix,
@@ -22,34 +22,63 @@ def dataReader(file_path):
 if __name__ == '__main__':
     file_path = '../../../circularDataProcess/data/'
     file_name = 'simulating_data_ECG.csv'
+    #file_path = '../../Benchmarks/Time Series Data/Intel Lab Data/Temperature/'
+    #file_name = 'normalSeqs_data.csv'
     normal_data = dataReader(file_path + file_name)
-    num_sensors, num_seqs = normal_data.shape
+    num_seqs, num_sensors = normal_data.shape
     
     # Parameters configuration
     _lambda = 0.95 # forgetting factor
     gamma = 0.90 # probability of misclassification 
-    f = True # anomaly flag, true means anomalous
-    alpha = 0 # 
-    beta = 0 #
+    f = False # classification label, False means not belongs to any existing clusters
+    alpha = 1 # 
+    beta = 1 #
 
-    k = 200 # num of initial samples
-    init_samples = normal_data[:k][:]
+    start = 50 # skip the first 50 points
+    k = 100 # num of initial samples
+    init_samples = normal_data[start:k][:]
     mean = init_samples.mean(axis = 0)
-    print init_samples
-    cov = np.cov(init_samples)
-    print cov
+    cov = np.cov(init_samples.T)
+    #print cov
     cov_inv = np.linalg.inv(cov) # We update the inverse of the covariance in practice!
-    print cov_inv
-    '''
-    for i in range(k, num_seqs):
-        new_intance = normal_data[k]
-        # classify the new instance, f is the anomaly flag
-        f = classifier.classify(gamma, mean, cov_inv, new_instance)
+    #print cov_inv
+    clusters = [] # a list of arries, each array stores instances of one cluster
+    clusters_means = [] # a list of cluster means
+    clusters_cov_invs = [] # a list of the inverses of cluster covariances
+    chi_square_dists = [] # a list of the chi square distance of each cluster
 
-        if f == False: # the new data is not anomalous
-            # update the model and parameters
-            alpha, beta, chi, mean, cov_inv = onlineUpdate.update( \
-                alpha, beta, chi,  _lambda, i, mean, cov_inv, new_instance)
-        #else:
-            # add a new cluster
-'''
+    clsuters.append(init_samples)
+    chi_square_dists.append()
+    clusters_means.append(mean) 
+    clusters_cov_invs.append(cov_inv)
+
+    # initialize the online updator 
+    updators = []
+    updators.append(onlineUpdator(alpha, beta, _lambda, mean, cov_inv))
+    # initialize the classifier
+    classifier = anomalyDetector(gamma)
+    print "Start update from the ", k, "th instance to the ", num_seqs,"th instance" 
+    for i in range(k, num_seqs):
+        print 'Iteration ', i-k
+        new_instance = normal_data[i]
+        # classify the new instance, f is the cluster label
+        f = classifier.classify(clusters_means, clusters_cov_invs, new_instance)
+        if f == False: # need a new cluster
+            clusters.append(new_instance) # add new cluster
+            clusters_means.append(new_instance) # add new instance as the mean of new cluster
+            clusters_cov_invs.append(np.ones([num_sensors, num_sensors]))
+            updators.append(onlineUpdator(alpha, beta, _lambda, 
+                new_instance, np.ones([num_sensors, num_sensors])))
+            clusters.append(new_instance)
+        else: # update ohe existing cluster
+            mean, cov_inv = updators[f].update(i, new_instance)
+            cluster[f] = np.vstack(clusters[f], new_instance)
+            clusters_means[f] = mean
+            clusters_cov_invs[f] = cov_inv
+    #print clusters_means
+    print '# of clusters is: ', len(clusters_means)
+
+
+#######
+##Try to uses python classes to define each cluster, 
+#then use for loop in the main function
--- Algorithms/ewIDCAD/onlineUpdate.py
+++ Algorithms/ewIDCAD/onlineUpdate.py
@@ -1,22 +1,31 @@
 from __future__ import division
 import numpy as np
 
-class onlineUpdate():
-    def update(alpha, beta, chi, _lambda, k, m, cov_inv, x):
+class onlineUpdator():
+    def __init__(self, alpha, beta, _lambda, mean, cov_inv, chi = 0):
+        self.alpha = alpha
+        self.beta = beta
+        self.chi = chi
+        self._lambda = _lambda
+        self.mean = mean
+        self.cov_inv = cov_inv
+
+    def update(self, k, x):
         # here x should be 1*W array where W is the number of features
 
         # update parameters
-        alpha = alpha*_lambda + 1
-        beta = beta*_lambda**2 + 1
-        A = cov_inv/(_lambda * chi) # Note that this is the A_{k-1}
-        chi = alpha/(alpha**2-beta)
+        self.alpha = self.alpha*self._lambda + 1
+        self.beta = self.beta*self._lambda**2 + 1
+        A = self.cov_inv/(self._lambda * self.chi) # Note that this is the A_{k-1}
+        self.chi = self.alpha/(self.alpha**2-self.beta)
 
         # update mean
-        m = m + (x - m)/alpha
+        self.mean = self.mean + (x - self.mean)/self.alpha
 
         # update inverse of covariance
-        num = A*((x - m).T)*(x - m)*A
-        den = 1+((x - m).T)*A*(x - m)
-        cov_inv = (1/chi) * (A - num / den )
-        return alpha, beta, chi, m, cov_inv
+        num = A*((x - self.mean).T)*(x - self.mean)*A
+        den = 1+((x - self.mean).T)*A*(x - self.mean)
+        self.cov_inv = (1/self.chi) * (A - num / den )
+
+        return self.mean, self.cov_inv
 
