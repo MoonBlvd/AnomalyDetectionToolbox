57225a806cf29f1a8d3a29ccf8a9f402526cfafb
brianyao <brianyao@umich.edu>
2017-03-13 15:47:26 -0400

    from mac to linux

--- Algorithms/ewIDCAD/ewIDCAD.py
+++ Algorithms/ewIDCAD/ewIDCAD.py
@@ -2,6 +2,16 @@ import numpy as np
 import csv
 from clusters import cluster
 import matplotlib.pyplot as plt
+#from featureExtractor import feature_extractor
+
+# Parameters configuration
+_lambda = 0.95 # forgetting factor
+gamma = 0.90 # probability of misclassification 
+f = False # classification label, False means not belongs to any existing clusters
+alpha = 1 # 
+beta = 1 #
+start = 50 # skip the first 50 points
+k = 100 # num of initial samples
 
 def find_min_distance(clusters, new_instance):
     min_distance = np.inf
@@ -12,7 +22,7 @@ def find_min_distance(clusters, new_instance):
             min_index = i
     return min_distance, min_index
 
-def dataReader(file_path):
+def read_data(file_path):
     # The read-in data should be a N*W matrix,
     # where N is the length of the time sequences,
     # W is the number of sensors/data features
@@ -28,37 +38,43 @@ def dataReader(file_path):
             i += 1
     return data
 
+def read_data_fields(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file, delimiter = ',')
+        for line in reader:
+            for i in range (0, len(line)):
+                if line[i] == '':
+                    line = line[0:i]
+                    break
+            return line
+
 if __name__ == '__main__':
+    # read data and fields
     file_path = '../../Benchmarks/Time Series Data/'
-    file_name = 'simulating_data_ECG.csv'
-    #file_path = '../../Benchmarks/Time Series Data/Intel Lab Data/Temperature/'
-    #file_name = 'normalSeqs_data.csv'
-    normal_data = dataReader(file_path + file_name)
-    normal_data = normal_data[:,2:4]
-    #print normal_data.shape
+    data_file_name = 'simulating_data_ECG.csv'
+    fields_file_name = 'fields.csv'
+    normal_data = read_data(file_path + data_file_name)
+    fields = read_data_fields(file_path + fields_file_name)
+
+    # extract data features
+    num_seqs, num_sensors = normal_data.shape
+    extractor = feature_extractor(num_sensors)
+    features = extractor.find_depend_data(normal_data, fields)
+    #normal_data = normal_data[:,2:4]
     #normal_data = np.append(normal_data, normal_data[:,[0]]**2, 1)
     #normal_data = np.append(normal_data, normal_data[:,[1]]**2, 1)
-    print normal_data
-    num_seqs, num_sensors = normal_data.shape
-    
-    # Parameters configuration
-    _lambda = 0.95 # forgetting factor
-    gamma = 0.90 # probability of misclassification 
-    f = False # classification label, False means not belongs to any existing clusters
-    alpha = 1 # 
-    beta = 1 #
 
-    start = 50 # skip the first 50 points
-    k = 100 # num of initial samples
+
+    # get initial data
     init_samples = normal_data[start:k][:]
     mean = init_samples.mean(axis = 0)
     cov = np.cov(init_samples.T)
-    #print cov
 
     # initialize the online updator 
     clusters = []
     clusters.append(cluster(init_samples, alpha, beta, _lambda, mean, cov, gamma))
     print "Start update from the ", k, "th instance to the ", num_seqs,"th instance" 
+
     for i in range(k, num_seqs):
         print 'Iteration ', i-k
         new_instance = normal_data[i][:]
@@ -94,3 +110,4 @@ if __name__ == '__main__':
 #######
 ##Try to uses python classes to define each cluster, 
 #then use for loop in the main function
+
