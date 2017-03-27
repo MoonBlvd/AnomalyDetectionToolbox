a254e34244bb754d1a247cfc62a0354b1704208f
brianyao <brianyao@umich.edu>
2017-03-27 14:38:19 -0400

    change glot ellipse

--- Algorithms/ewIDCAD/plot_ellipse.m
+++ Algorithms/ewIDCAD/plot_ellipse.m
@@ -1,6 +1,6 @@
 clear;clc;
-%raw_data = csvread('../../Benchmarks/Time Series Data/IBRL/IBRL_18_25000-28800_temp_hum.csv');
-raw_data = csvread('../../Benchmarks/Time Series Data/Car_Simulation/Car_RollOverData_1_6D.csv');
+raw_data = csvread('../../Benchmarks/Time Series Data/IBRL/IBRL_18_25000-28800_temp_hum.csv');
+%raw_data = csvread('../../Benchmarks/Time Series Data/Car_Simulation/Car_RollOverData_1_6D.csv');
 
 mean = csvread('new_mean.csv');
 covariance = csvread('new_cov.csv');
@@ -10,8 +10,8 @@ d = 2;
 data_d = size(mean,2);
 for i = 1: size(mean,1)
     mu = mean(i,1:d)';
-    %cov = covariance(data_d*(i-1)+1:data_d*(i-1)+d,1:d);
-    cov_inv = covariance_inv(data_d*(i-1)+1:data_d*(i-1)+d,1:d);
+    cov = covariance(data_d*(i-1)+1:data_d*(i-1)+d,1:d);
+    cov_inv = covariance_inv(data_d*(i-1)+1:data_d*(i-1)+d,1:d)/7.4;
     [V,D] = eig(cov_inv);
     lam1 = D(1,1);
     lam2 = D(2,2);
@@ -30,18 +30,19 @@ for i = 1: size(mean,1)
     angle = [0:500]*2*pi/np;
     R = [cos(theta),-sin(theta);sin(theta), cos(theta)];
     pts = [mu(1);mu(2)]*ones(size(angle)) + R*[cos(angle)*a; sin(angle)*b];
-    if i == 1
-        plot(raw_data(1:5,1), raw_data(1:5,2), '*');
-        hold on;
-    else
-        plot(raw_data(i,1), raw_data(i,2),'*');
-        hold on
-    end
     
-    plot(pts(1,:), pts(2,:), 'r');
-    xlim([20,70]);
-    ylim([20,45]);
-    pause(0.05)
-    hold off
-end
-        
+    if(mod(i,100)==0)
+        plot(raw_data(1:i,1), raw_data(1:i,2),'*');
+        hold on
+    
+        plot(pts(1,:), pts(2,:), 'r');
+    %xlim([-5,10]);
+    %ylim([-5,10]);
+        xlim([0,70]);
+        ylim([0,45]);
+    
+        pause(0.5)
+        hold off
+
+    end
+end    
